3e1df31229dbb469b233e74841ed3cfdcafaec8d
hrobeers <hansrobeers@gmail.com>
2018-05-14 20:14:01 +0200

    Add license info to relevant sources and client banner

--- libexec/eyoc-client-raw.sh
+++ libexec/eyoc-client-raw.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
+
+#    Copyright (C) 2018  hrobeers
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 host=$1
 port=$2
 encode=${3:-cat}
 decode=${4:-cat}
 username=$USER
+
+read -d '' banner <<- EOF
+#####################################################################
+    eyoc-client  Copyright (C) 2018  hrobeers
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under under the terms of the GNU General Public License v3.
+#####################################################################
+EOF
 
 eyoc_fifo_in=$(mktemp /tmp/eyoc.fifo.XXXXXXXXX --dry-run)
 mkfifo $eyoc_fifo_in
@@ -12,7 +37,7 @@ send_cmd="while read line; do echo \"<$username> \$line\" 1>&2; clear; done 2> $
 recv_cmd="cat $eyoc_fifo_in | foreach-line.sh \"$encode\" | nc $host $port | foreach-line.sh \"$decode\""
 
 tmux \
-    new-session "$recv_cmd" \; \
+    new-session "echo \"$banner\" && $recv_cmd" \; \
     split-window "$send_cmd" \; \
     resize-pane -y 2
 
